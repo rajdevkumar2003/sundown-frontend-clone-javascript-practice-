3b601bf50f0c4599ce7e9604a219effb8a08f222
Rajeev <rajeevkumarjbo2003@gmail.com>
2025-01-13 11:47:03 +0530

    added page4

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+var elemC=document.querySelector("#elem-container");
+var fixed=document.querySelector("#fixed-container");
+
+
+elemC.addEventListener("mouseenter",function(){
+    fixed.style.display="block"
 });
+
+elemC.addEventListener("mouseleave",function(){
+    fixed.style.display="none"
+});
+
+var elems=document.querySelectorAll(".elem");
+elems.forEach((e)=>{
+    e.addEventListener("mouseenter",function(){
+        var img=e.getAttribute("data-image");
+        fixed.style.backgroundImage=`url(${img})`;
+    });
+})
